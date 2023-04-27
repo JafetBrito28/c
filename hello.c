04f3f8909228926d74736e7f017e5cd87d93529c
Jafet Brito <67519851+JafetBrito28@users.noreply.github.com>
2023-04-27 16:23:24 -0600

    create hello.c
    cow say create hello.c

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+   // printf() muestra la cadena dentro de la cita
+   // printf() displays the string inside quotation
+   printf("Hack the planet");
+   return 0;
+}
